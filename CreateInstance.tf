6dff20768c0105f15892cc32ff3ae07fa6b98fed
Arshadn123 <88405271+Arshadn123@users.noreply.github.com>
2021-09-20 23:19:52 -0400

    Update CreateInstance.tf
    rm keys

--- CreateInstance.tf
+++ CreateInstance.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  access_key = "AKIA4OMIOQNKZZPCNFDQ"
+  access_key = ""
   secret_key = "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"
   region = "us-east-1"
 }
  resource "aws_instance" "MyfirstInstance" {
-     ami        = "ami-0dfe175154343661a"
+     ami        = ""
      instance_type = "t2.micro"
  }
 
